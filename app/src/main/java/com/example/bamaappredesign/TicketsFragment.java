716fd3c68894150d9a3754d28042d9bec2312f37
Nicholas Brunelle <45525985+nickbrunelle@users.noreply.github.com>
2019-03-19 13:54:00 -0500

    Error correction with nonnull parameter syntax

--- app/src/main/java/com/example/bamaappredesign/TicketsFragment.java
+++ app/src/main/java/com/example/bamaappredesign/TicketsFragment.java
@@ -1,10 +1,13 @@
 package com.example.bamaappredesign;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class TicketsFragment extends Fragment {
 
@@ -14,7 +17,7 @@ public class TicketsFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState)
     {
         // Inflate the layout for this fragment
