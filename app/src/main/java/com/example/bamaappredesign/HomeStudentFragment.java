d6ca47ecacc3cd2ee9b80a492d504520cd2c977b
Ethan Johnson <ekjohnson4@crimson.ua.edu>
2019-03-26 08:18:18 -0500

    Added OnClick Methods to modules

--- app/src/main/java/com/example/bamaappredesign/HomeStudentFragment.java
+++ app/src/main/java/com/example/bamaappredesign/HomeStudentFragment.java
@@ -3,12 +3,12 @@ package com.example.bamaappredesign;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -20,8 +20,6 @@ public class HomeStudentFragment extends Fragment {
     FirebaseUser user;
     ViewPager viewPager;
     MyCustomPagerAdapter myCustomPagerAdapter;
-    private EditText mEditText;
-    //private List<RssFeedModel> mFeedModelList;
     int images[] = {R.drawable.slide1, R.drawable.slide2, R.drawable.slide3};
     String strings[] = {"Headline 1", "Headline 2","Headline 3"};
 
@@ -47,8 +45,41 @@ public class HomeStudentFragment extends Fragment {
         myCustomPagerAdapter = new MyCustomPagerAdapter(Objects.requireNonNull(getActivity()), images, strings);
         viewPager.setAdapter(myCustomPagerAdapter);
 
-        //mEditText =  view.findViewById(R.id.textView);
-        //new FetchFeedTask().execute((Void) null);
+        //Tickets Card
+        CardView card_view = view.findViewById(R.id.card1);
+        card_view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.flMain, new TicketsFragment());
+                ft.addToBackStack(null);
+                ft.commit();
+            }
+        });
+
+        //ActionCard Card
+        CardView card_view2 = view.findViewById(R.id.card2);
+        card_view2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.flMain, new ActionCardFragment());
+                ft.addToBackStack(null);
+                ft.commit();
+            }
+        });
+
+        //News Card
+        CardView card_view3 = view.findViewById(R.id.card3);
+        card_view3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.flMain, new NewsFragment());
+                ft.addToBackStack(null);
+                ft.commit();
+            }
+        });
 
         return view;
     }
