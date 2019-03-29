6cbd9fa814bb442ebd9c4c263de724bb2729c862
Ethan Johnson <ekjohnson4@crimson.ua.edu>
2019-03-28 19:48:41 -0500

    Made some optimizations to home pages

--- app/src/main/java/com/example/bamaappredesign/HomeVisitorFragment.java
+++ app/src/main/java/com/example/bamaappredesign/HomeVisitorFragment.java
@@ -1,9 +1,13 @@
 package com.example.bamaappredesign;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -42,6 +46,41 @@ public class HomeVisitorFragment extends Fragment {
         viewPager = view.findViewById(R.id.viewPager);
         myCustomPagerAdapter = new MyCustomPagerAdapter(Objects.requireNonNull(getActivity()), images, strings);
         viewPager.setAdapter(myCustomPagerAdapter);
+
+        //Events Card
+        CardView card_view = view.findViewById(R.id.card4);
+        card_view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.flMain, new EventsFragment());
+                ft.addToBackStack(null);
+                ft.commit();
+            }
+        });
+
+        //Shop Card
+        CardView card_view2 = view.findViewById(R.id.card5);
+        card_view2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("https://www.universitysupplystore.com/");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+        //News Card
+        CardView card_view3 = view.findViewById(R.id.card6);
+        card_view3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.flMain, new NewsFragment());
+                ft.addToBackStack(null);
+                ft.commit();
+            }
+        });
 
         return view;
     }
