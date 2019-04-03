de28f5a7f2407b80ef0b29e133e876fd4b94884c
Tyler Gillam <tgillam12@gmail.com>
2019-04-03 15:44:55 -0500

    Added null checks to events page

---
+++ app/src/test/java/com/example/bamaappredesign/EventTest.java
@@ -0,0 +1,13 @@
+package com.example.bamaappredesign;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class EventTest {
+    @Test
+    public void testEventTitle() {
+        Event a = new Event("Awesome event", "This is an event", "Gorgas", "04/03/2019");
+        assertEquals(a.getTitle(), "Awesome event");
+    }
+}
