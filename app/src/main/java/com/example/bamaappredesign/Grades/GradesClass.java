265b68e394f2f7a97d77ecacf77aaf4a8f143ca7
Ethan Johnson <ekjohnson4@crimson.ua.edu>
2019-04-09 15:34:31 -0500

    Class averages are displayed on Grades page and are color coded

--- app/src/main/java/com/example/bamaappredesign/Grades/GradesClass.java
+++ app/src/main/java/com/example/bamaappredesign/Grades/GradesClass.java
@@ -2,12 +2,17 @@ package com.example.bamaappredesign.Grades;
 
 public class GradesClass {
     private String cls;
+    private double grd;
 
-    public GradesClass(String cls){
+    public GradesClass(String cls, double grd){
         this.cls = cls;
+        this.grd = grd;
     }
 
     public String getCls(){
         return cls;
+    }
+    public double getGrd(){
+        return grd;
     }
 }
