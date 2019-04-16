6cb91d2c099446d9af8d71800ca9463b03e420d2
Ethan Johnson <ekjohnson4@crimson.ua.edu>
2019-04-16 14:34:47 -0500

    Further refactoring of Transportation page

--- app/src/main/java/com/example/bamaappredesign/Transportation/TransportationFragment.java
+++ app/src/main/java/com/example/bamaappredesign/Transportation/TransportationFragment.java
@@ -1,6 +1,7 @@
 package com.example.bamaappredesign.Transportation;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,18 +14,16 @@ import android.webkit.WebViewClient;
 import com.example.bamaappredesign.R;
 
 public class TransportationFragment extends Fragment {
-    private WebView mWebView;
-
     public TransportationFragment() {
         // Required empty public constructor
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_campus_map, container, false);
-        mWebView = view.findViewById(R.id.campus_map_webview);
+        WebView mWebView = view.findViewById(R.id.campus_map_webview);
 
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
