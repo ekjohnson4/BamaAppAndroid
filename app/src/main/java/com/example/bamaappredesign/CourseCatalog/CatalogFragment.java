4b189ea7179b53d21890f327ce996800e5aaaa05
Ethan Johnson <ekjohnson4@crimson.ua.edu>
2019-04-20 14:42:46 -0500

    Quick start of pulling data for course catalog

--- app/src/main/java/com/example/bamaappredesign/CourseCatalog/CatalogFragment.java
+++ app/src/main/java/com/example/bamaappredesign/CourseCatalog/CatalogFragment.java
@@ -1,23 +1,65 @@
 package com.example.bamaappredesign.CourseCatalog;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.bamaappredesign.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CatalogFragment extends Fragment {
+    FirebaseFirestore db;
 
     public CatalogFragment() {
         // Required empty public constructor
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_catalog, container, false);
+        View v = inflater.inflate(R.layout.fragment_catalog, container, false);
+
+        //Initialize variables
+        db = FirebaseFirestore.getInstance();
+        final Spinner spinner = v.findViewById(R.id.subjects);
+        final List<String> subjects = new ArrayList<>();
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, subjects);
+
+        //Retrieve subjects, currently hardcoded for Spring 2019
+        FirebaseFirestore rootRef = FirebaseFirestore.getInstance();
+        CollectionReference subjectsRef = rootRef.collection("courseCatalog").document("Spring2019").collection("subject");
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        subjectsRef.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        String abb = document.getId();
+                        String subject = document.getString("name");
+                        subjects.add(abb + " - " + subject);
+                    }
+                    adapter.notifyDataSetChanged();
+                }
+            }
+        });
+        return v;
     }
 }
