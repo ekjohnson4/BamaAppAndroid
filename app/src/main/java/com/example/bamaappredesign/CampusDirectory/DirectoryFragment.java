4877fb6f21298760f8a932b32ce2fc9ee84f1787
Ethan Johnson <ekjohnson4@crimson.ua.edu>
2019-04-23 20:15:59 -0500

    Directory stays in app now

--- app/src/main/java/com/example/bamaappredesign/CampusDirectory/DirectoryFragment.java
+++ app/src/main/java/com/example/bamaappredesign/CampusDirectory/DirectoryFragment.java
@@ -1,41 +1,36 @@
 package com.example.bamaappredesign.CampusDirectory;
 
-
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.example.bamaappredesign.R;
 
-
-/**
- * A simple {@link Fragment} subclass.
- */
 public class DirectoryFragment extends Fragment {
-
 
     public DirectoryFragment() {
         // Required empty public constructor
     }
 
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-
         View view =  inflater.inflate(R.layout.fragment_directory, container, false);
         WebView mWebView = view.findViewById(R.id.directoryWB);
 
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-        mWebView.loadUrl("https://www.ua.edu/directory/");//"https://www.ua.edu/directory/?l=&f=michael&p=&d=&t=all&_action=submit");
+        mWebView.setWebChromeClient(new WebChromeClient());
+        mWebView.setWebViewClient(new WebViewClient());
+        mWebView.loadUrl("https://www.ua.edu/directory/");
 
         return view;
     }
-
 }
